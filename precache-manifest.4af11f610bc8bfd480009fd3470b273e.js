2e590c68aaef8028213cf4b7d005555263e89bb2
Dawit <davelet120@gmail.com>
2020-07-24 00:50:46 -0700

    Updates

---
+++ precache-manifest.4af11f610bc8bfd480009fd3470b273e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94e65a4e6c9f09b4908152770343430",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "d24b58c3210c93d798ac",
+    "url": "/RecipeApp/static/css/main.d4202cee.chunk.css"
+  },
+  {
+    "revision": "fdb99fe3b47925c3e29a",
+    "url": "/RecipeApp/static/js/2.9d445451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.9d445451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24b58c3210c93d798ac",
+    "url": "/RecipeApp/static/js/main.ce493bac.chunk.js"
+  },
+  {
+    "revision": "e0e4aab33597159ee711",
+    "url": "/RecipeApp/static/js/runtime-main.793232f2.js"
+  }
+]);
